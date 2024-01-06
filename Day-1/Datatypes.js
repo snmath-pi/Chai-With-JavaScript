9c94c1299f1a18105e4b2a1f5b573aa0c3eaee96
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-01-06 16:37:20 +0530

    new commit

---
+++ Day-1/Datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// can't be used like this in node.js (but can be used like this in browser)
+// alert("Hellow");
+// alert(3 + 6);
+
+console.log(3 + 3);
+console.log("Saksham");
+
+console.log(69 - 
+    69); // not a good practice
+
+let name = "saksham";
+let age = 3;
+let isLoggedIn = true;
+let love = null;
+/*
+Primitive Data Types
+number => 2 ^ 53
+bigint
+boolean
+string
+null [Standalone value]
+undefined (different from null)
+symbol => uniqueness
+
+Objects:
+*/
+
+console.table([name, age, isLoggedIn, love]);
+console.log(typeof "saksham");
+console.log(typeof null);
+console.log(typeof 69);
+console.log(typeof undefined);
+
