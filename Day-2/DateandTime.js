22db4657eba93c9e111dc670a7fe16e1c26d9ca0
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-01-07 20:29:18 +0530

    commit

---
+++ Day-2/DateandTime.js
@@ -0,0 +1,36 @@
+// sari dates Jan1 1970 se calculate hoti hai javascript mein
+// inhone temporal suggest kari hai api
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+
+// date object hai javascript mein
+
+// let uu = new Date(2024, 2, 10);
+// let uu = new Date(2024, 2, 10, 5, 3);
+let uu = new Date("2024-10-13");
+
+
+// console.log(uu.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(uu.getTime());
+
+console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+
+
+
+
+
+
---
+++ Day-2/Numbers.js
@@ -0,0 +1,41 @@
+const x = 400;
+
+const y = new Number(100);
+/*
+console.log(x);
+console.log(y);
+
+
+console.log(x.toString());
+console.log(x.toString().length);
+// number of decimal places
+console.log(x.toFixed(2));
+
+const otherNum = 23432.2342;
+console.log(otherNum.toPrecision(4));
+console.log(otherNum.toPrecision(6));
+
+
+const tata = 1000000;
+console.log(tata.toLocaleString('en-IN'));
+
+const ll = Number.MAX_VALUE;
+console.log(ll);
+
+
+*/
+
+// console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.floor(4/5));
+console.log(Math.ceil(8.1320));
+
+console.log(Math.min(1, 2, 3, 4));
+
+console.log(Math.ceil(1 + Math.random()*10));
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
