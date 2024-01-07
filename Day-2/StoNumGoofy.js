6ad330d48f694cd0ba2e5f112c934762e42548be
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-01-07 19:03:28 +0530

    day2

---
+++ Day-2/StoNumGoofy.js
@@ -0,0 +1,19 @@
+let value = 100;
+value = -value;
+
+console.log(value);
+
+console.log(2 ** 9);
+
+let s = "Saksham";
+let S = "Negi";
+console.log(s + S );
+// jab ek string declare ho jaye usse pehle ka sab normal 
+// but uske baad ka sab string concatination
+console.log("1" + 2 + 2 + 2 + 2 + 2 + 8 + 9);
+
+console.log(11 + 13 + "2" + 4 + "5");
+
+let J = 10, K = 10;
+console.log(++J);
+console.log(K++);
