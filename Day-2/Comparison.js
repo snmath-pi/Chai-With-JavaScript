ea9c98119a27e36c9bbd58b6c81a6946c860e257
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-01-12 20:33:52 +0530

    f

---
+++ Day-2/Comparison.js
@@ -0,0 +1,19 @@
+
+console.log("3" > 1);
+console.log("03" > 1);
+
+// unpredictable results
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0); // null ko 0 mein convert kar diya
+
+// === -> strict check (checks value and also checks datatypes)
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+// data types panga karte isliye soch samagh kar use kar
+// is tarah ke conversion avoid kar
+
+
---
+++ Day-2/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(uu.getTime());
